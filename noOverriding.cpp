b2d1a42fd50a46dccf69acab73992a35fa2cf2bd
nabilasaf <nabilasafitri861@gmail.com>
2024-05-28 15:38:31 +0700

    membuat turunan kelas daro baseClass ke kelas derivedClass

--- noOverriding.cpp
+++ noOverriding.cpp
@@ -8,3 +8,10 @@ class baseClass {
     }
 };
 
+class derivedClass : public baseClass {
+    public :
+       void perkenalan() {
+        cout << "Hallo saya function dari deroved Class";
+       }
+};
+
